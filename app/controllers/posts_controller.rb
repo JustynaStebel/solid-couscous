62d58960cc50c179498b81c6b3a55b02689b4e4d
JustynaStebel <justyna.stebel@netguru.pl>
2016-09-17 18:47:25 +0200

    Change redirection target

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to posts_path
     else
       render 'new'
     end
